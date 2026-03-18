6098cd6f79ed78a4710db36be3328775a1b9cc2a
Camille Galladjov <ujinjinjin@outlook.com>
2026-03-18 19:12:35 +0100

    remove soup

--- src/Soup.Api/Exceptions/BaseApiException.cs
+++
@@ -1,3 +0,0 @@
-namespace Soup.Api.Exceptions;
-
-public abstract class BaseApiException(string? message) : ApplicationException(message);
--- src/Soup.UnitTests/UnitTest1.cs
+++
@@ -1,9 +0,0 @@
-namespace Soup.UnitTests;
-
-public class UnitTest1
-{
-	[Fact]
-	public void Test1()
-	{
-	}
-}
