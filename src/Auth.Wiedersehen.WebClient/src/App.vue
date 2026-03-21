387d4de0151b3cb06b15451fe804c241401274c2
Camille Galladjov <ujinjinjin@outlook.com>
2026-03-21 22:23:01 +0100

    setup docker and i18n

--- src/Auth.Wiedersehen.WebClient/src/App.vue
+++ src/Auth.Wiedersehen.WebClient/src/App.vue
@@ -3,12 +3,14 @@ import ThemeToggle from '@/components/primitives/theme-toggle/ThemeToggle.vue'
 import { Toaster } from '@/components/ui/sonner'
 import { Button } from '@/components/ui/button'
 import { toast } from 'vue-sonner'
+import LanguageSwitcher from '@/components/primitives/language-switcher/LanguageSwitcher.vue'
 </script>
 
 <template>
   <ThemeToggle />
   <h1 class="text-3xl font-bold underline">Hello world!</h1>
-  <Button @click="() => toast('My first toast')">Click me</Button>
+  <Button @click="() => toast('My first toast')">{{ $t('sign-in:header') }}</Button>
+  <LanguageSwitcher />
   <Toaster />
 </template>
 
