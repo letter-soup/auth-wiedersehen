25131b1c1b6714854cfd2a4791ebffca9dd82613
Camille Galladjov <ujinjinjin@outlook.com>
2026-03-21 21:17:25 +0100

    tailwind configs

--- src/Auth.Wiedersehen.WebClient/src/App.vue
+++ src/Auth.Wiedersehen.WebClient/src/App.vue
@@ -1,11 +1,12 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <div class="bg-gray-100 dark:bg-gray-900">
+    <h1 class="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  </div>
 </template>
 
 <style scoped></style>
