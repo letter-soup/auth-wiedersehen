17b6799025484211c3869de3c7e6871a2f912b44
Camille Galladjov <ujinjinjin@outlook.com>
2026-07-23 23:40:59 +0200

    #8 update migrations  post package upgrade

--- src/Auth.Wiedersehen/Database/Migrations/ConfigurationDb/20260723213908_UpgradePackages.Designer.cs
+++ src/Auth.Wiedersehen/Database/Migrations/ConfigurationDb/20260723213908_UpgradePackages.Designer.cs
@@ -4,17 +4,20 @@ using System.Collections.Generic;
 using Duende.IdentityServer.EntityFramework.DbContexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 #nullable disable
 
-namespace Auth.Wiedersehen.Migrations.ConfigurationDb
+namespace Auth.Wiedersehen.Database.Migrations.ConfigurationDb
 {
     [DbContext(typeof(ConfigurationDbContext))]
-    partial class ConfigurationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260723213908_UpgradePackages")]
+    partial class UpgradePackages
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
